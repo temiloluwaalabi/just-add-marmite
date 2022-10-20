30fb74fb057ef36bcec1515dc9216f3043ef1d31
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2022-10-20 22:05:48 +0100

    chnaged

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -39,6 +40,8 @@ export const getStaticProps = async ({ params }) => {
 
 }
 export default function RecipeDetails({recipe}) {
+  if(!recipe) return <Skeleton />
+
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
   console.log({recipe});
   return (
